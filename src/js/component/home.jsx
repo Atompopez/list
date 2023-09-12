97e929f3c666c3ca33a18419d618c64a36f34a8a
Johan Lopez <johanandrey0210@gmail.com>
2023-09-12 10:51:53 +0000

    second commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  const [list, setList] = useState([]);
+  const [inputValue, setInputValue] = useState("");
+
+  const agregar = (e) => {
+    if (e.key === "Enter" && inputValue.trim() !== "") {
+      setList([...list, inputValue]);
+      setInputValue("");
+    }
+  };
+
+  const cancelar = (k) => {
+    const copiaList = [...list];
+    copiaList.splice(k, 1);
+    setList(copiaList);
+  };
+
+  return (
+    <div className="container">
+      <div className="textoPrincipal">todos</div>
+	  <div className="lista">
+		<input
+			type="text"
+			onKeyDown={agregar}
+			onChange={(e) => setInputValue(e.target.value)}
+			placeholder="Whats needs to be done?"
+			value={inputValue}
+		/>
+		<ul class="list-group">
+			{list.map((item, key) => (
+			<li class="list-group-item" key={key}>
+				{item}
+				<button onClick={() => cancelar(key)}>Cancelar</button>
+			</li>
+			))}
+			<li class="list-group-item">{list.length} item left</li>
+		</ul>
+		
+	  </div>
+    </div>
+  );
 };
 
 export default Home;
