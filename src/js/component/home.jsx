4dcb3ba01d9bbe9566e9b75fb1362aaaf76e326d
Johan Lopez <johanandrey0210@gmail.com>
2023-09-15 09:03:25 +0000

    second commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -20,25 +20,27 @@ const Home = () => {
   return (
     <div className="container">
       <div className="textoPrincipal">todos</div>
-	  <div className="lista">
-		<input
-			type="text"
-			onKeyDown={agregar}
-			onChange={(e) => setInputValue(e.target.value)}
-			placeholder="Whats needs to be done?"
-			value={inputValue}
-		/>
-		<ul class="list-group">
-			{list.map((item, key) => (
-			<li class="list-group-item" key={key}>
-				{item}
-				<button onClick={() => cancelar(key)}>Cancelar</button>
-			</li>
-			))}
-			<li class="list-group-item">{list.length} item left</li>
-		</ul>
-		
-	  </div>
+      <div className="lista">
+        <input
+          className="entrada"
+          type="text"
+          onKeyDown={agregar}
+          onChange={(e) => setInputValue(e.target.value)}
+          placeholder="Whats needs to be done?"
+          value={inputValue}
+        />
+        <ul class="list-group">
+          {list.map((item, key) => (
+            <li class="list-group-item" id='item' key={key}>
+              {item}
+              <svg onClick={cancelar} xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16">
+                <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
+              </svg>
+            </li>
+          ))}
+          <li class="list-group-item" id="contador">{list.length} item left</li>
+        </ul>
+      </div>
     </div>
   );
 };
