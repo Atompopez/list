56d291cb3a26a23665d5f8bf220aaed1f1a92b13
Johan Lopez <johanandrey0210@gmail.com>
2023-09-17 09:55:22 +0000

    add get and include functionality to the list

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,13 +1,21 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const Home = () => {
   const [list, setList] = useState([]);
   const [inputValue, setInputValue] = useState("");
 
+  useEffect(()=>{
+    fetch('https://playground.4geeks.com/apis/fake/todos/user/johan')
+    .then((response) => response.json())
+    .then((data)=> setList(data))
+  },[])
+
   const agregar = (e) => {
     if (e.key === "Enter" && inputValue.trim() !== "") {
-      setList([...list, inputValue]);
+      let newId = list.length+1;
+      setList([...list, {"done":false , "id":newId ,"label": inputValue}]);
       setInputValue("");
+      console.log(list)
     }
   };
 
@@ -29,16 +37,16 @@ const Home = () => {
           placeholder="Whats needs to be done?"
           value={inputValue}
         />
-        <ul class="list-group">
+        <ul className="list-group">
           {list.map((item, key) => (
-            <li class="list-group-item" id='item' key={key}>
-              {item}
-              <svg onClick={()=>{cancelar(key)}} xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16">
+            <li className="list-group-item" id='item' key={key}>
+              {item.label}
+              <svg onClick={()=>{cancelar(key)}} xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-x" viewBox="0 0 16 16">
                 <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
               </svg>
             </li>
           ))}
-          <li class="list-group-item" id="contador">{list.length} item left</li>
+          <li className="list-group-item" id="contador">{list.length} item left</li>
         </ul>
       </div>
     </div>
