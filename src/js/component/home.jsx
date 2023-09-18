3be47eb4fe291e06c06dbcd55911d832a299cb0b
Johan Lopez <johanandrey0210@gmail.com>
2023-09-18 00:02:03 +0000

    add trash button

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -28,12 +28,18 @@ const Home = () => {
       setList([...list, {"done":false , "id":newId ,"label": inputValue}]);
       setInputValue("");
     }
-  }
+  };
 
   const cancelar = (k) => {
     const copiaList = [...list];
     copiaList.splice(k, 1);
     setList(copiaList);
+  };
+
+  const limpiar = () => {
+    const copiaList = [];
+    setList([...copiaList])
+    console.log("limpiar")
   };
 
   return (
@@ -60,6 +66,11 @@ const Home = () => {
           <li className="list-group-item" id="contador">{list.length} item left</li>
         </ul>
       </div>
+      <button onClick={()=>{limpiar()}} type="button" class="btn btn-danger">
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3-fill" viewBox="0 0 16 16">
+          <path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5Zm-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5ZM4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06Zm6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528ZM8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5Z"/>
+        </svg>
+      </button>
     </div>
   );
 };
